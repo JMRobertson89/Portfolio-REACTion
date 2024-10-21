a4df060ee09974cf75cbd6d3f105901bbde50358
JMRobertson89 <joshrobertson1618@gmail.com>
2024-10-20 22:34:45 -0400

    finished header and footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+function Footer() {
+    return (
+        <footer>
+            <div className="right">
+                <a href="https://github.com/JMRobertson89" target="_blank">  
+                    <i className="fa-brands fa-github"></i>
+                    <span>GitGub</span>
+                </a>
+                <a href="https://www.linkedin.com/in/jrobertson-io/" target="_blank" rel="noopener noreferrer">
+                    <i className="fa-brands fa-linkedin"></i>
+                    <span>LinkedIn</span>
+                </a>
+                <a href="./Assets/files/Joshua Robertson Resume.pdf" target="_blank">  
+                    <i className="fa-solid fa-file"></i>
+                    <span>Resume</span>
+                </a>
+                <a href="mailto:jrobertson.io">
+                    <i className="fa-solid fa-envelope"></i>
+                    <span>Email</span>
+                </a>
+            </div>
+        </footer>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import NavBar from './NavBar';
+
+function Header(props) {
+    const { currentPage, handlePageChange } = props;
+
+    return (
+        <>
+            <section>
+                <header className='left'>
+                    <div>
+                        <a href="/">
+                            <span className="first-name">Joshua</span>
+                            <span className="last-name">Robertson</span>
+                        </a>
+                    </div>
+                <div>
+                    <NavBar currentPage={currentPage} handlePageChange={handlePageChange} >
+                    </NavBar>
+                </div>
+                </header>
+            </section>
+
+            <section className='headerBack' >
+                <h2 className='headerWel'>HELLO!</h2>
+                <p className='PWel'>
+                Hi there.
+                </p>
+            </section>
+        </>
+    );
+};
+
+export default Header
